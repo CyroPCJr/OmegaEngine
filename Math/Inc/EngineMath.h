23d07bf919b43f36252905f4c69c3f0d3e206da7
Cyro Paulino da Costa Jr <cpcmetal@hotmail.com>
2019-10-31 00:30:38 -0700

    [Omega] - Implement basics functions to Matrix4. - Implement unity test for all those functions from Matrisx4 - Think how to do the Inverse function on EngineMath ( Inverse Matrix ).

--- Math/Inc/EngineMath.h
+++ Math/Inc/EngineMath.h
@@ -37,21 +37,23 @@ namespace Omega::Math
 		return { v / lenght };
 	}
 
-	inline Matrix4 Transpose(const Matrix4& m)
-	{
-		/*
+
+	/*
+	Original:
 		 _11, _12, _13, _14;
 		 _21, _22, _23, _24;
 		 _31, _32, _33, _34;
 		 _41, _42, _43, _44;
 
-
+	Transpose:
 		 _11, _21, _31, _41;
 		 _12, _22, _32, _42;
 		 _13, _23, _33, _43;
 		 _14, _24, _34, _44;
-		
+
 		*/
+	inline Matrix4 Transpose(const Matrix4& m)
+	{
 		Matrix4 result;
 		result._11 = m._11;
 		result._12 = m._21;
@@ -77,33 +79,14 @@ namespace Omega::Math
 
 	constexpr float Determinant(const Matrix4& m)
 	{
-		/*
-		m00	m01	m02	m03
-		m10	m11	m12	m13
-		m20	m21	m22	m23
-		m30	m31	m32	m33
-
-
-		_11, _12, _13, _14;
-		 _21, _22, _23, _24;
-		 _31, _32, _33, _34;
-		 _41, _42, _43, _44;
-		
-		*/
-		//const float det = (m._14 * m._23 * m._32*m._41) - (m._13*m.)
-		///*m03* m12* m21* m30*/ - m02 * m13 * m21 * m30 - m03 * m11 * m22 * m30 + m01 * m13 * m22 * m30 +
-		//m02 * m11 * m23 * m30 - m01 * m12 * m23 * m30 - m03 * m12 * m20 * m31 + m02 * m13 * m20 * m31 +
-		//m03 * m10 * m22 * m31 - m00 * m13 * m22 * m31 - m02 * m10 * m23 * m31 + m00 * m12 * m23 * m31 +
-		//m03 * m11 * m20 * m32 - m01 * m13 * m20 * m32 - m03 * m10 * m21 * m32 + m00 * m13 * m21 * m32 +
-		//m01 * m10 * m23 * m32 - m00 * m11 * m23 * m32 - m02 * m11 * m20 * m33 + m01 * m12 * m20 * m33 +
-		//m02 * m10 * m21 * m33 - m00 * m12 * m21 * m33 - m01 * m10 * m22 * m33 + m00 * m11 * m22 * m33;
-
-
-
-		return {};
+		float cofactor1 = m._11 * ((m._22 * ((m._33 * m._44) - (m._34 * m._43))) - ((m._23 * ((m._32 * m._44) - (m._34 * m._42)))) + ((m._24 * ((m._32 * m._43) - (m._33 * m._42)))));
+		float cofactor2 = m._12 * ((m._21 * ((m._33 * m._44) - (m._34 * m._43))) - ((m._23 * ((m._31 * m._44) - (m._34 * m._41)))) + ((m._24 * ((m._31 * m._43) - (m._33 * m._41)))));
+		float cofactor3 = m._13 * ((m._21 * ((m._32 * m._44) - (m._34 * m._42))) - ((m._22 * ((m._31 * m._44) - (m._34 * m._41)))) + ((m._24 * ((m._31 * m._42) - (m._32 * m._41)))));
+		float cofactor4 = m._14 * ((m._21 * ((m._32 * m._43) - (m._33 * m._42))) - ((m._22 * ((m._31 * m._43) - (m._33 * m._41)))) + ((m._23 * ((m._31 * m._42) - (m._32 * m._41)))));
+		float det = cofactor1 - cofactor2 + cofactor3 - cofactor4;
+		return det;
 	}
-
-	constexpr Matrix4 Inverse(const Matrix4& m)
+	inline Matrix4 Inverse(const Matrix4& m)
 	{
 		return {};
 	}
