40c3422710a15a7666499dc40b1cbe88011305f0
Cyro Paulino da Costa Jr <cpcmetal@hotmail.com>
2019-11-02 23:12:38 -0700

    [Omega] - Add Inverse function in EngineMathTest.cpp .

--- UnitTests/MathTest/EngineMathTest.cpp
+++ UnitTests/MathTest/EngineMathTest.cpp
@@ -87,7 +87,47 @@ namespace MathTest
 						1.f, 0.f, 5.f, 0.f };
 			const float det = Determinant(A);
 			Assert::AreEqual(det, 30.f);
+		}
 
+		TEST_METHOD(InverseMatrixTest)
+		{
+			/*
+				actual:
+				3 7 3 6
+				7 1 9 9
+				9 8 3 6
+				6 8 2 10
+
+				expected:
+				-19/94	1/94	15/94	3/188
+				10/47	-3/47	2/47	-9/94
+				193/846	23/282	1/846	-119/564
+				-40/423	4/141	-55/423	59/282
+			*/
+			Matrix4 matrix{ 3.f, 7.f, 3.f, 6.f,
+							7.f, 1.f, 9.f, 9.f,
+							9.f, 8.f, 3.f, 6.f,
+							6.f, 8.f, 2.f, 10.f };
+			const Matrix4 inverseResult = Inverse(matrix);
+			Assert::AreEqual(inverseResult._11, -0.202127665f);
+			Assert::AreEqual(inverseResult._12, 0.0106382975f);
+			Assert::AreEqual(inverseResult._13, 0.159574464f);
+			Assert::AreEqual(inverseResult._14, 0.0159574468f);
+
+			Assert::AreEqual(inverseResult._21, 0.212765962f);
+			Assert::AreEqual(inverseResult._22, -0.0638297871f);
+			Assert::AreEqual(inverseResult._23, 0.0425531901f);
+			Assert::AreEqual(inverseResult._24, -0.0957446843f);
+
+			Assert::AreEqual(inverseResult._31, 0.228132382f);
+			Assert::AreEqual(inverseResult._32, 0.0815602839f);
+			Assert::AreEqual(inverseResult._33, 0.00118203310f);
+			Assert::AreEqual(inverseResult._34, -0.210992903f);
+
+			Assert::AreEqual(inverseResult._41, -0.0945626497f);
+			Assert::AreEqual(inverseResult._42, 0.0283687934f);
+			Assert::AreEqual(inverseResult._43, -0.130023643f);
+			Assert::AreEqual(inverseResult._44, 0.209219858f);
 		}
 	};
 }
